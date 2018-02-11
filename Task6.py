3d616216c36224ae21fa2295f870add719dcaaa6
lilliputten <lilliputten@yandex.ru>
2018-02-12 00:07:02 +0300

    Task6 code style changed

--- Task6.py
+++ Task6.py
@@ -1,28 +1,48 @@
-# test list:
-#a = [12, -30, 23, -43, 51]
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# parameters
+listLength = 2  # array length
+
 # input...
-a = []
-for n in range(20):
-    y = input()
-    a.append(y)
+
+# test list
+# list = [12, -30, 23, -43, 51]
+
+# create empty list:
+list = []
+
+# adding values from input...
+for n in range(listLength):
+    y = input('Enter number #' + str(n + 1) + ': ')
+    list.append(y)
+
+# show list
+print "Enetered list:", list
+
 # initialize variables...
-n = 0
-m = 0
-mm = 0
-allMin = 0
-# evaluating average...
-for b in a:
-    if b < 0:
-        allMin += b
-        n += 1
-    print(b)
-mm = float(allMin) / n
+negCount = 0  # negative numbers count
+negSumm = 0  # negative numbers summ
+
+# calculating negatives count & summ...
+for val in list:
+    if val < 0:
+        negSumm += val
+        negCount += 1
+
+# evaluating negatives negAverage
+negAverage = 0
+# if positive number...
+if negCount > 0:
+    # ...then get average
+    negAverage = float(negSumm) / negCount
+
 # modifiyng list...
-b = 0
-while b < len(a):
-    a[b] -= mm
-    b += 1
-# result:
-print "allMin =", allMin
-print "arithmetic mean =", mm
-print(a)
+for n in range(len(list)):
+    list[n] -= negAverage
+
+# results:
+print "negCount:", negCount
+print "negSumm:", negSumm
+print "negAverage:", negAverage
+print "Changed list:", list
