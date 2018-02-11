9694fd3cac49fee8211c6bdde1792b4d41519444
lilliputten <lilliputten@yandex.ru>
2018-02-11 22:31:30 +0300

    Comment

---
+++ Task5.py
@@ -0,0 +1,18 @@
+# input
+print "Enter number a(1;2 dflt)"
+a = input()
+print "Enter number h(3;2 dflt)"
+h = input()
+print "Enter number P(5;14 dflt)"
+P = input()
+# Go working...
+s = 0
+n = 0
+while True:
+    s = s + ( a + n * h )
+    print str(n) + ":" + str(s)
+    if s > P:
+        break
+    n = n + 1
+# result
+print(n)
