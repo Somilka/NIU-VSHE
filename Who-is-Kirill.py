e6d95971c3eb5caa8e8346a0401d606f5d81afcf
Somilka <jack.munin@yandex.ru>
2018-02-18 03:48:59 +0300

    HARD

--- Who-is-Kirill.py
+++ Who-is-Kirill.py
@@ -1,6 +1,6 @@
 # Who is Kirill? v.1
 a = input("Who is Kirill?")
-list.append(a)
+list.append(a)  # не знаю, что это значит, но это было в task6.py, и я решил попробовать))
 lox = 1
 if a = 1:
     print "Krill lox 100%"
