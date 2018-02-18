d4b63a7b2b698908ac7ca77359a193f0b19d71b8
Somilka <jack.munin@yandex.ru>
2018-02-18 13:34:23 +0300

    Done

--- Who-is-Kirill.py
+++ Who-is-Kirill.py
@@ -1,7 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # Who is Kirill? v.1
-a = input("Who is Kirill?")
-list.append(a)  # не знаю, что это значит, но это было в task6.py, и я решил попробовать))
-lox = 1
-if a = 1:
-    print "Krill lox 100%"
+
+# Спрашиваем пользователя, лох ли Кирилл
+a = raw_input("Kirill loh?\n")
+
+# Проверяем, что возвращает a
+# print type(a), a
+
+p = raw_input("Skolko procentov?\n")
+
+# учитывая ответ, содержащийся в a, выясняем, кто такой Кирилл
+if a == "da":
+    print "Kirill loh", p, "%"
+elif a == "net":
+    print "Krill ne loh", p, "%"
+elif a != "net":
+    # print "Who is kirill?"
+    # b = raw_input("Who is Kirill?\n")
+    print a, p, "%"
+
+# p = raw_input("Skolko procentov?\n")
+# print a, p, "%"
 # памагити, очень сложно((
